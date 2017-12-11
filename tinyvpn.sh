8f46c9949a6c91292e447271e9002142ad4bc082
linyuhc001 <linyuhc001@gmail.com>
2017-12-11 20:22:06 +0800

    Update tinyvpn.sh

--- tinyvpn.sh
+++ tinyvpn.sh
@@ -2,7 +2,8 @@
 # This script will be set up tinyvpn
 # by fw867
 # 注意将serverip改成成你vps的地址，使用ifconfig查看，有些vps网卡上是没有公网ip的，比如谷歌云，虽然能申请固定公网ip,但是网卡是内网ip,你就必须设置成内网的ip,直接设置成固定外网ip会导致无法访问外网
-export serverip=$(ifconfig eth0 | grep "inet" | sed 's/^.*addr://g' | sed s/Bcast.*$//)
+
+export serverip=ifconfig eth0 | grep "inet" | sed 's/^.*addr://g' | sed s/Bcast.*$//
 export tinyvpn_port=4031
 export udp2raw_port=4032
 export local_addr=172.16.0.0
